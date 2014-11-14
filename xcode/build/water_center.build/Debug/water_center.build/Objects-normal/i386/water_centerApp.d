90b7a8a4cc59f1dfdd7dc9869706d26d6949a4dc
John N Choi <jnc331@nyu.edu>
2014-11-14 01:40:04 -0500

    first commit

---
+++ xcode/build/water_center.build/Debug/water_center.build/Objects-normal/i386/water_centerApp.d
@@ -0,0 +1,7 @@
+dependencies: \
+  /Users/johnnamuchoi/Documents/2014\ Fall\ ITP/Big\ Screen/cinder/water_center/src/water_centerApp.cpp \
+  ../../../../../cinder/cinder_0.8.6_mac/include/cinder/app/AppNative.h \
+  ../../../../../cinder/cinder_0.8.6_mac/include/cinder/gl/Texture.h \
+  /Users/johnnamuchoi/Documents/2014\ Fall\ ITP/Big\ Screen/cinder/water_center/xcode/WaterdropCon.h \
+  ../../../../../cinder/cinder_0.8.6_mac/include/cinder/Rand.h \
+  /Users/johnnamuchoi/Documents/2014\ Fall\ ITP/Big\ Screen/cinder/water_center/xcode/Waterdrop.h
