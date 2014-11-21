867c85f31e4481295f9517fc29b6a162c6469850
John N Choi <jnc331@nyu.edu>
2014-11-21 12:01:12 -0500

    timeInterval

--- src/water_centerApp.cpp
+++ src/water_centerApp.cpp
@@ -20,7 +20,7 @@ public:
     WaterdropCon mWaterdropCon;
     float a[2];
     bool signal;
-    float amt;
+    float waterLifeSpan01,waterLifeSpan02;
 };
 
 void water_centerApp::prepareSettings( Settings *settings){
@@ -37,7 +37,6 @@ void water_centerApp::setup()
     
     a[0]=0;
     a[1]=0;
-    amt=0;
     mWaterdropCon.setup();
 }
 
@@ -50,6 +49,7 @@ void water_centerApp::mouseMove( MouseEvent event )
     if(a[0]>300 && a[1] <= 300){
         
         signal=true;
+        waterLifeSpan01= getElapsedSeconds()*100.f;
     }
 }
 
@@ -58,12 +58,10 @@ void water_centerApp::update()
     mWaterdropCon.update();
     mWaterdropCon.removeWaterdrop();
     if (signal) {
-        if (amt<30) {
+        waterLifeSpan02 = getElapsedSeconds()*100.f;
+        float loopSignal = waterLifeSpan02-waterLifeSpan01;
+        if ((int) loopSignal%95 < 30){
             mWaterdropCon.addWaterdrop();
-            amt++;
-        }else{
-            amt=0;
-            signal=false;
         }
     }
 }
